4e3a2afe6fa5ac6146c07b8a78fd5e31b5574ff2
OshkarVTec <a01275287@tec.mx>
2024-06-26 14:26:26 -0600

    fix paddings

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ function App() {
 	return (
 		<>
 			<div className="flex">
-				<div className="w-1/2 h-screen flex justify-center p-96">
-					<ul className="list-none text-3xl flex flex-col gap-4 font-bold">
-						<li>Portfolio</li>
-						<li>Projects</li>
-						<li>Experience</li>
-						<li>Skills</li>
-						<li>Education</li>
-						<li>Contact</li>
+				<div className="w-1/2 h-screen flex justify-center items-center">
+					<ul className="list-none text-3xl font-bold h-auto">
+						<li className="py-2">Portfolio</li>
+						<li className="py-2">Projects</li>
+						<li className="py-2">Experience</li>
+						<li className="py-2">Skills</li>
+						<li className="py-2">Education</li>
+						<li className="py-2">Contact</li>
 					</ul>
 				</div>
-				<div className="w-1/2 h-screen bg-black animate-slideInLeft flex flex-col justify-center items-center">
+				<div className="w-1/2 h-screen bg-black animate-slideInLeft flex justify-center items-center">
 					<div>
 						<p className="text-7xl text-white font-bold">Oskar Villa</p>
 						<p className="text-3xl text-white">Software Developer</p>
