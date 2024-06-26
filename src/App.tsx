db62a9b8fae50915399b57a94cda084e37f8452b
OshkarVTec <a01275287@tec.mx>
2024-06-26 14:21:11 -0600

    change font weight

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 		<>
 			<div className="flex">
 				<div className="w-1/2 h-screen flex justify-center p-96">
-					<ul className="list-none text-3xl flex flex-col gap-4">
+					<ul className="list-none text-3xl flex flex-col gap-4 font-bold">
 						<li>Portfolio</li>
 						<li>Projects</li>
 						<li>Experience</li>
@@ -14,7 +14,7 @@ function App() {
 				</div>
 				<div className="w-1/2 h-screen bg-black animate-slideInLeft flex flex-col justify-center items-center">
 					<div>
-						<p className="text-7xl text-white">Oskar Villa</p>
+						<p className="text-7xl text-white font-bold">Oskar Villa</p>
 						<p className="text-3xl text-white">Software Developer</p>
 					</div>
 				</div>
