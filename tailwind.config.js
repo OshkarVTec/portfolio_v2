930a591392e2ca9d1c8297fbc882b2c9cfe855b1
OshkarVTec <a01275287@tec.mx>
2024-06-26 14:09:59 -0600

    add slide animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				slideInLeft: {
+					"0%": { opacity: 1, transform: "translateX(100%)" },
+					"100%": { opacity: 1, transform: "translateX(0)" },
+				},
+			},
+			animation: {
+				slideInLeft: "slideInLeft .5s ease-in-out forwards",
+			},
+		},
+		plugins: [],
 	},
-	plugins: [],
 };
