0483e5ced20fa0f7ec685f45383a287594892155
OshkarVTec <a01275287@tec.mx>
2024-06-26 19:42:42 -0600

    add buttons

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -1,6 +1,8 @@
 import Subtitle from "../common/Subtitle";
 import Slider from "react-slick";
 import "./Projects.css";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import React from "react";
 
 export default function Projects() {
 	const settings = {
@@ -14,12 +16,31 @@ export default function Projects() {
 		className: "center",
 		centerPadding: "100px",
 	};
+
+	const slider = React.useRef<Slider | null>(null);
+
+	const leftArrow = () => {
+		console.log(slider?.current?.slickPrev());
+		slider?.current?.slickPrev();
+	};
+
+	const rightArrow = () => {
+		slider?.current?.slickNext();
+	};
 	return (
 		<div className="h-screen">
 			<div>
 				<Subtitle>Projects</Subtitle>
 			</div>
-			<Slider {...settings}>
+			<div className="flex justify-between px-96 z-[904] ">
+				<button onClick={leftArrow} className="z-[910]">
+					<AiOutlineLeft size={50} />
+				</button>
+				<button onClick={rightArrow} className="z-[910]">
+					<AiOutlineRight size={50} />
+				</button>
+			</div>
+			<Slider ref={slider} {...settings} className="z-[800]">
 				<div>
 					<div className="border-2 border-black h-96 w-full bg-red-100">
 						<h3>1</h3>
