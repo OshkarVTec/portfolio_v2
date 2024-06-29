1342cb90a68a1b94b882cc3a401f4d98fc2199ea
OshkarVTec <a01275287@tec.mx>
2024-06-28 20:18:22 -0600

    add margin

--- src/common/Subtitle.tsx
+++ src/common/Subtitle.tsx
@@ -1,9 +1,7 @@
 import { ReactNode } from "react";
 
 export default function Subtitle({ children }: SubtitleProps) {
-	return (
-		<h2 className="mx-14 p-4 text-black font-bold text-5xl">{children}</h2>
-	);
+	return <h2 className="m-14 p-4 text-black font-bold text-5xl">{children}</h2>;
 }
 
 interface SubtitleProps {
