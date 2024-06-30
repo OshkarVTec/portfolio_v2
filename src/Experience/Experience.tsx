21933971d4b32b9976cc4f8373e5055a1baa9792
OshkarVTec <a01275287@tec.mx>
2024-06-30 16:52:48 -0600

    change section height

--- src/Experience/Experience.tsx
+++ src/Experience/Experience.tsx
@@ -2,7 +2,7 @@ import Subtitle from "../common/Subtitle";
 
 export default function Experience() {
 	return (
-		<div className="h-screen">
+		<div className="">
 			<Subtitle>Experience</Subtitle>
 			<div className="flex flex-wrap justify-around">
 				<div className="border-2 border-black w-[45%] m-4">1</div>
