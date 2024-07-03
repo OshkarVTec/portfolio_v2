8c7833cc3e007f45d42c67ceb8e0f1bcc99e1baa
OshkarVTec <a01275287@tec.mx>
2024-07-02 19:59:00 -0600

    center icons and text

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -7,7 +7,10 @@ export default function Contact() {
 			<Subtitle>Contact</Subtitle>
 			<ul className="flex justify-around">
 				{SocialData.map((social) => (
-					<li key={social.platform}>
+					<li
+						key={social.platform}
+						className="flex flex-col items-center justify-center"
+					>
 						<a href={social.link}>{social.icon}</a>
 						<h3>{social.platform}</h3>
 					</li>
