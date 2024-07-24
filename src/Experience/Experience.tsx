da52a95943cd73a741dd1318878eed8ecfd0a392
OshkarVTec <a01275287@tec.mx>
2024-07-23 22:22:36 -0600

    add experience cards

--- src/Experience/Experience.tsx
+++ src/Experience/Experience.tsx
@@ -1,13 +1,34 @@
 import Subtitle from "../common/Subtitle";
+import { experienceData } from "../data/experience.ts";
 
 export default function Experience() {
 	return (
 		<section id="experience">
 			<Subtitle>Experience</Subtitle>
 			<div className="flex flex-wrap justify-around">
-				<div className="border-2 border-black w-[45%] m-4">1</div>
-				<div className="border-2 border-black w-[45%] m-4">2</div>
-				<div className="border-2 border-black w-[45%] m-4">3</div>
+				{experienceData.map((experienceItem) => (
+					<div
+						className="border-2 border-black w-[45%] m-4 p-4"
+						key={experienceItem.name}
+					>
+						<div className="my-4">
+							<div className="flex justify-between">
+								<h3 className="text-lg font-bold">{experienceItem.name}</h3>
+								<p className="text-sm">{experienceItem.date}</p>
+							</div>
+							<h4 className="text-md">{experienceItem.position}</h4>
+						</div>
+						<p className="text-sm">{experienceItem.description}</p>
+
+						<ul className="list-disc py-4 px-8">
+							{experienceItem.bullets.map((bullet) => (
+								<li key={bullet} className="text-sm ">
+									{bullet}
+								</li>
+							))}
+						</ul>
+					</div>
+				))}
 			</div>
 		</section>
 	);
